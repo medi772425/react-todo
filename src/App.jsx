56c398ed562c5ef2aa6caf7b72e19b815a596d9d
medi772425 <75742857+medi772425@users.noreply.github.com>
2022-07-15 21:41:45 +0900

    Edit App

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,31 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div>
+        <input placeholder="ToDoを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のToDo</p>
+        <div>
+          <ul>
+            <li>a</li>
+            <button>完了</button>
+            <button>削除</button>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <p>完了したToDo</p>
+        <div>
+          <ul>
+            <li>a</li>
+            <button>戻す</button>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 };
